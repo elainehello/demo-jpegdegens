401fa1959076dc43dd9e2ad6e9c959c447fd1287
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-25 17:48:00 +0200

    dep chai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 // Import Hardhat Waffle plugin for advanced testing and assertions with ethers.js
 import "@nomiclabs/hardhat-waffle";
+// Import Hardhat Ethers plugin to use ethers.js within Hardhat tasks and tests
+import "@nomiclabs/hardhat-ethers";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
---
+++ test/HelloWorld.ts
@@ -0,0 +1,6 @@
+// Import Hardhat Ethers plugin to enable ethers.js in Hardhat environment
+import "@nomiclabs/hardhat-ethers";
+// Import ethers object from Hardhat for contract deployment and interaction
+import { ethers } from "hardhat";
+// Import Chai expect for assertions in tests
+import { expect } from "chai";
