a3498f4442fdd36e39c8765bfc9890800371254d
elaine <195331169+elainehello@users.noreply.github.com>
2025-08-25 17:33:46 +0200

    upd

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,6 @@
+// Import Hardhat Waffle plugin for advanced testing and assertions with ethers.js
+import "@nomiclabs/hardhat-waffle";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
